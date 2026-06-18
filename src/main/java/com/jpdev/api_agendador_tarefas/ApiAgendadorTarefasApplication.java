debd6a4d8e01ae124b0b3492aec1d31653417b72
João Paulo Lima <jplopapa.dev@gmail.com>
2026-06-18 12:00:21 -0300

    Commit inicial

---
+++ src/main/java/com/jpdev/api_agendador_tarefas/ApiAgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.jpdev.api_agendador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgendadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpdev/api_agendador_tarefas/ApiAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpdev.api_agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
